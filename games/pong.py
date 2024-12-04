f53dae031342be89d5fb21a5200a9d04031c124b
Norbert <kieler103@gmail.com>
2024-12-04 21:24:48 +0100

    test

--- games/pong.py
+++ games/pong.py
@@ -8,3 +8,4 @@ class PongWebSocketHandler(Rag2WebsocketHandler):
     def send_data(self, receivedData):
         move = random.choice([0, 1, -1])
         self.write_message(json.dumps({'move': move}))
+        # dzieki dziala
