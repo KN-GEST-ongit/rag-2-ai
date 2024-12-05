b066fb747cdc3a6ddf244c128a782a3a6afe6a1f
enkayem <enkayem69@gmail.com>
2024-12-05 01:57:18 +0100

    test

--- games/pong.py
+++ games/pong.py
@@ -7,7 +7,7 @@ from Rag2WebsocketHandler import Rag2WebsocketHandler
 class PongWebSocketHandler(Rag2WebsocketHandler):
     def send_data(self, receivedData):
         move = random.choice([0, 1, -1])
-        self.write_message(json.dumps({'move': move}))
+        self.write_message(json.dumps({'move': move})) #test
 
 class PongBot(Rag2WebsocketHandler):
     def send_data(self, receivedData):
