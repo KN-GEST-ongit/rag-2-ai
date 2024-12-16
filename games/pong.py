b57a157f855f8dadb292122ac6bac664ef95cfe6
enkayem <enkayem69@gmail.com>
2024-12-16 11:33:23 +0100

    feat: added HappyJumpBot

--- games/pong.py
+++ games/pong.py
@@ -22,3 +22,50 @@ class PongBot(Rag2WebsocketHandler):
             else:
                 move = -1
         self.write_message(json.dumps({'move': move}))
+
+class HappyJumpBot(Rag2WebsocketHandler):
+    def send_data(self, receivedData):
+        state = receivedData['state']
+        if not state.get('isGameStarted', False):
+            self.write_message(json.dumps({'start': 1}))
+            print("Gra jeszcze się nie rozpoczęła. Wysyłam komendę START.")
+            return
+
+        # Pozycje gracza
+        player_x = state['playerX']
+        player_y = state['playerY']
+        player_speed_y = state['playerSpeedY']
+        platforms = state['platforms']
+
+        move = 0  # -1: w lewo, 1: w prawo, 0: stop
+        jump = 0  # 0: nie skacz, 1: skacz
+
+        # Znajdowanie najbliższej platformy powyżej lub poniżej gracza
+        closest_platform = None
+        min_distance = float('inf')
+        for platform in platforms:
+            if platform['y'] > player_y:  # Platforma poniżej gracza
+                distance = abs(platform['y'] - player_y)
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_platform = platform
+
+        # Decyzje bota
+        if closest_platform:
+            platform_x = closest_platform['x']
+            platform_y = closest_platform['y']
+
+            # Ruch w poziomie (również w powietrzu)
+            if player_x < platform_x - 5:  # Celuj trochę na środek platformy
+                move = 1  # Idź w prawo
+            elif player_x > platform_x + 5:
+                move = -1  # Idź w lewo
+            else:
+                move = 0  # Gracz jest wystarczająco nad platformą
+
+            # Skakanie, jeśli platforma jest wystarczająco blisko
+            if (platform_y - player_y) < 100 and player_speed_y >= 0:  # Platforma wyżej i gracz spada
+                jump = 1
+
+        # Wysyłanie akcji do serwera
+        self.write_message(json.dumps({'move': move, 'jump': jump}))
--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
-from games.pong import PongWebSocketHandler, PongBot
-
+from games.pong import PongWebSocketHandler, PongBot, HappyJumpBot
 
 def make_app():
     return tornado.web.Application([
         (r"/ws/pong/", PongWebSocketHandler), #http://localhost:8001/ws/pong/
         (r"/ws/pong-bot/", PongBot), #http://localhost:8001/ws/pong-bot/
+        (r"/ws/happyjump-bot/", HappyJumpBot),  #http://localhost:8001/ws/happyjump-bot/
     ])
 
 
