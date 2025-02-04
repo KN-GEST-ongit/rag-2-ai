ef305f0f0163dd6c39ad0fa6acde25c9d1379221
enkayem <enkayem69@gmail.com>
2025-02-04 20:58:24 +0100

    feat: #18 added new HappyJumpBot

--- src/bots.py
+++ src/bots.py
@@ -64,6 +64,42 @@ class SkiJumpBot(BaseHandler):
 class HappyJumpBot(BaseHandler):
     def send_prediction(self, data: dict):
         state = data['state']
-        move = random.choice([0, 1, -1])
-        # TODO
-        self.write_message(json.dumps({'jump': 1, 'move': move}))
+        move = 0
+        jump = 0
+
+        if not state['isGameStarted']:
+            jump = 1
+        else:
+            platforms_below = [p for p in state['platforms'] if p['y'] > state['playerY']]
+
+            if platforms_below:
+                nearest_platform = min(platforms_below, key=lambda p: p['y'])
+                player_left, player_right = state['playerX'], state['playerX'] + 30
+                platform_left, platform_right = nearest_platform['x'], nearest_platform['x'] + 100
+
+                #Ruch bota, jeśli występuje przynajmniej jedna platforma ruchoma
+                if state['movingPlatforms'] > 0:
+                    platform_offset = nearest_platform['directionX'] * state['platformSpeed']
+                    platform_left += platform_offset
+                    platform_right += platform_offset
+
+                if state['playerSpeedY'] < 0:
+                    if platform_left > player_right:
+                        move = 1
+                    elif platform_right < player_left:
+                        move = -1
+                else:
+                    if platform_left > player_right:
+                        move = 1
+                        if abs(player_right - platform_left) <= 3:
+                            jump = 1
+                    elif platform_right < player_left:
+                        move = -1
+                        if abs(player_left - platform_right) <= 3:
+                            jump = 1
+                    else:
+                        jump = int(state['playerSpeedY'] > 0)
+            else:
+                jump = 1
+
+        self.write_message(json.dumps({'jump': jump, 'move': move}))
