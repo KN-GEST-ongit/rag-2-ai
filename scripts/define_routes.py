df6611d00cf7eb58ec769154bfb11171610cece5
IgnacyJ <ignacy.janus00@gmail.com>
2026-05-29 18:18:36 +0200

    feat: #32
    Add Abalone NNUE WebSocket agent with AlphaBeta search engine,
    Zobrist hashing, transposition table, killer moves and NNUE evaluation.

--- scripts/define_routes.py
+++ scripts/define_routes.py
@@ -1,6 +1,7 @@
 from typing import List, Tuple, Type
 from src.api import RoutesHandler
 from src.bots import PongBot, FlappybirdBot, SkiJumpBot, HappyJumpBot, PacManBot
+from src.agents.web_abalone import AbaloneAgent
 from src.handlers import AiHandler
 from src.agents.web_pong import PongAgent
 from src.agents.web_flappy_bird import FlappyBirdAgent
@@ -16,7 +17,7 @@ from scripts.load_models import (
     trpo_fb,
     dqn_hj,
     ppo_hj,
-    trpo_hj
+    trpo_hj,
 )
 
 
@@ -77,22 +78,29 @@ def define_routes() -> List[Tuple[str, Type, dict]]:
         (r"/ws/pacman/pacman-bot/", PacManBot)
     ]
 
+    abalone_routes = [
+        (r"/ws/abalone/abalone-nnue/", AbaloneAgent),
+    ]
+
     pong_endpoint = (r"/ws/pong/routes/", RoutesHandler, dict(routes=pong_routes))
     flappybird_endpoint = (r"/ws/flappybird/routes/", RoutesHandler, dict(routes=flappybird_routes))
     skijump_endpoint = (r"/ws/skijump/routes/", RoutesHandler, dict(routes=skijump_routes))
     happyjump_endpoint = (r"/ws/happyjump/routes/", RoutesHandler, dict(routes=happyjump_routes))
     pacman_endpoint = (r"/ws/pacman/routes/", RoutesHandler, dict(routes=pacman_routes))
+    abalone_endpoint = (r"/ws/abalone/routes/", RoutesHandler, dict(routes=abalone_routes))
 
     routes += pong_routes
     routes += flappybird_routes
     routes += skijump_routes
     routes += happyjump_routes
     routes += pacman_routes
+    routes += abalone_routes
 
     routes.append(pong_endpoint)
     routes.append(flappybird_endpoint)
     routes.append(skijump_endpoint)
     routes.append(happyjump_endpoint)
     routes.append(pacman_endpoint)
+    routes.append(abalone_endpoint)
 
     return routes
